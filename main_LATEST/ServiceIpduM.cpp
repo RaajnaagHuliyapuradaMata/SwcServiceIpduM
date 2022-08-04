cc1113d33fbff2fd4b03070d551321432df973e3
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-04 19:54:26 +0200

    Update: AUTOSAR basic software service module for interaction layer protocol data unit multiplexer

---
+++ main_LATEST/ServiceIpduM.cpp
@@ -0,0 +1,150 @@
+/******************************************************************************/
+/* File   : ServiceIpduM.cpp                                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+#include "ServiceIpduM.hpp"
+#include "infServiceIpduM_Imp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define IPDUM_AR_RELEASE_VERSION_MAJOR                                         4
+#define IPDUM_AR_RELEASE_VERSION_MINOR                                         3
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(IPDUM_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible IPDUM_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(IPDUM_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible IPDUM_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+VAR(module_ServiceIpduM, IPDUM_VAR) ServiceIpduM;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, IPDUM_CODE) module_ServiceIpduM::InitFunction(
+      CONSTP2CONST(ConstModule_TypeAbstract, IPDUM_CONST,       IPDUM_APPL_CONST) lptrConstModule
+   ,  CONSTP2CONST(CfgModule_TypeAbstract,   IPDUM_CONFIG_DATA, IPDUM_APPL_CONST) lptrCfgModule
+){
+#if(STD_ON == ServiceIpduM_InitCheck)
+   if(
+         E_OK
+      != IsInitDone
+   ){
+#endif
+      if(
+            (NULL_PTR != lptrConstModule)
+         && (NULL_PTR != lptrCfgModule)
+      ){
+         lptrConst = (const ConstServiceIpduM_Type*)lptrConstModule;
+         lptrCfg   = lptrCfgModule;
+      }
+      else{
+#if(STD_ON == ServiceIpduM_DevErrorDetect)
+         ServiceDet_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
+#if(STD_ON == ServiceIpduM_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == ServiceIpduM_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  IPDUM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, IPDUM_CODE) module_ServiceIpduM::DeInitFunction(
+   void
+){
+#if(STD_ON == ServiceIpduM_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == ServiceIpduM_InitCheck)
+      IsInitDone = E_NOT_OK;
+   }
+   else{
+#if(STD_ON == ServiceIpduM_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  IPDUM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, IPDUM_CODE) module_ServiceIpduM::MainFunction(
+   void
+){
+#if(STD_ON == ServiceIpduM_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == ServiceIpduM_InitCheck)
+   }
+   else{
+#if(STD_ON == ServiceIpduM_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  IPDUM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, IPDUM_CODE) module_ServiceIpduM::dummy(
+   void
+){
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
