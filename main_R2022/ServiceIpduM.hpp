e8675d0c8ea59b3ed59f08e5f31ea47634851ddd
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-29 15:51:50 +0200

    Update: AUTOSAR basic software service module for interaction layer protocol data unit multiplexer

---
+++ main_R2022/ServiceIpduM.hpp
@@ -0,0 +1,68 @@
+#pragma once
+/******************************************************************************/
+/* File   : ServiceIpduM.hpp                                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "ConstServiceIpduM.hpp"
+#include "CfgServiceIpduM.hpp"
+#include "ServiceIpduM_core.hpp"
+#include "infServiceIpduM_Exp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class module_ServiceIpduM:
+      INTERFACES_EXPORTED_SERVICEIPDUM
+      public abstract_module
+   ,  public class_ServiceIpduM_Functionality
+{
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+      const ConstServiceIpduM_Type* lptrConst = (ConstServiceIpduM_Type*)NULL_PTR;
+      infServicePduRClient_Up infServicePduRClient_ServiceIpduM_Up;
+      infServicePduRClient_Lo infServicePduRClient_ServiceIpduM_Lo;
+
+   public:
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+      FUNC(void, SERVICEIPDUM_CODE) InitFunction(
+            CONSTP2CONST(ConstModule_TypeAbstract, SERVICEIPDUM_CONST,       SERVICEIPDUM_APPL_CONST) lptrConstModule
+         ,  CONSTP2CONST(CfgModule_TypeAbstract,   SERVICEIPDUM_CONFIG_DATA, SERVICEIPDUM_APPL_CONST) lptrCfgModule
+      );
+      FUNC(void, SERVICEIPDUM_CODE) DeInitFunction (void);
+      FUNC(void, SERVICEIPDUM_CODE) MainFunction   (void);
+      SERVICEIPDUM_CORE_FUNCTIONALITIES
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+extern VAR(module_ServiceIpduM, SERVICEIPDUM_VAR) ServiceIpduM;
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
