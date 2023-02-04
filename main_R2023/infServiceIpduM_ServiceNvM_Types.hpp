85117eb99569064cfd3da912863f9a96365e4181
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:29:11 +0100

    Update: AUTOSAR basic software service module for interaction layer protocol data unit multiplexer

--- main_R2023/infServiceIpduM_ServiceNvM_Types.hpp
+++ main_R2023/infServiceIpduM_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_ServiceIpduM_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_ServiceIpduM_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
